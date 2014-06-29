a2605e1d4add86dc8ab89f309e75235de61aef54
Sharon <seaustin@live.unc.edu>
2014-06-28 21:48:41 -0400

    small updates and tweaks

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -4,6 +4,35 @@
 *
 * Displays the homepage of the portfolio site
 *
-* /
+* */
 
-get_header(); ?>
+// get the options
+$slider_category = reactor_option('frontpage_slider_category', ''); ?>
+
+<?php get_header(); ?>
+
+	<div id="primary" class="site-content">
+    
+    	<?php reactor_content_before(); ?>
+  
+        <div class="row">
+          	<div class="<?php reactor_columns( 12 ); ?>">
+
+            </div><!-- .columns -->
+        </div><!-- .row -->
+  
+        <div id="content" role="main">
+        	<div class="row">        
+                <div class="<?php reactor_columns(); ?>">
+                
+                
+                </div><!-- .columns -->
+
+            </div><!-- .row -->
+        </div><!-- #content -->
+        
+        
+        
+	</div><!-- #primary -->
+
+
